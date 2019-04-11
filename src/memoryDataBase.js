9075c8fbc603e54cc75c1175bd432d91af1b815e
Daniel Henrique Fuzari <dhfuzari@gmail.com>
2019-04-10 22:21:03 -0300

    feat(src): basic http methods created for crud operations - memory database created

---
+++ src/memoryDataBase.js
@@ -0,0 +1,30 @@
+const sequence = {
+  _id: 1,
+  get id() {
+    return this._id++;
+  }
+}
+
+const products = {};
+
+function saveProduct(prod) {
+  if (!prod.id) prod.id = sequence.id;
+  products[prod.id] = prod;
+  return prod;
+}
+
+function getProduct(id) {
+  return products[id] || {};
+}
+
+function getProducts() {
+  return Object.values(products);
+}
+
+function deleteProduct(id) {
+  const prod = products[id];
+  delete products[id];
+  return prod;
+}
+
+module.exports = { saveProduct, getProduct, getProducts, deleteProduct };
---
+++ src/server.js
@@ -0,0 +1,44 @@
+// port means process - which process will answare the request
+const port = 3003;
+
+const express = require('express');
+const bodyParsser = require('body-parser');
+
+const app = express();
+const memoryDataBase = require('./memoryDataBase');
+
+app.use(bodyParsser.urlencoded({ extended: true }))
+
+app.get('/products', (req, res, next) => {
+  res.send(memoryDataBase.getProducts()) // Convert to JSON
+});
+
+app.get('/product/:id', (req, res, next) => {
+  res.send(memoryDataBase.getProduct(req.params.id))
+})
+
+app.post('/products', (req, res, next) => {
+  const product = memoryDataBase.saveProduct({
+    name: req.body.name,
+    price: req.body.price
+  });
+  res.send(product);
+});
+
+app.put('/product/:id', (req, res, next) => {
+  const product = memoryDataBase.saveProduct({
+    id: req.params.id,
+    name: req.body.name,
+    price: req.body.price
+  });
+  res.send(product);
+});
+
+app.delete('/product/:id', (req, res, next) => {
+  const product = memoryDataBase.deleteProduct(req.params.id);
+  res.send(product);
+});
+
+app.listen(port, () => {
+  console.log(`### Server listening to port 3003`);
+});
